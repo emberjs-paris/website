42db8772ce2e8e2c46770a266be7ce5641a6c2e2
Paul Chavard <git@paul.chavard.net>
2017-07-04 16:39:13 +0200

    enable service worker

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,15 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    'asset-cache': {
+      include: [
+        'assets/**/*'
+      ]
+    },
+    'esw-cache-fallback': {
+      patterns: [ '/' ],
+      version: '1' // Changing the version will bust the cache
+    },
     vendorFiles: {
       'jquery.js': null
     }
