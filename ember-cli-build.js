369b860271f0e82ac159fcbaf426556aa8c4a72a
Paul Chavard <git@paul.chavard.net>
2017-09-15 12:48:57 +0200

    upgrade ember-service-worker

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -11,7 +11,7 @@ module.exports = function(defaults) {
     },
     'esw-cache-fallback': {
       patterns: [ '/' ],
-      version: '3' // Changing the version will bust the cache
+      version: '4' // Changing the version will bust the cache
     },
     vendorFiles: {
       'jquery.js': null
