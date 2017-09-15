e7bcc622e4eacf03ea0cb0211a206dd21f8f2b6e
Paul Chavard <git@paul.chavard.net>
2017-09-15 15:13:55 +0200

    cleanup app tree

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,8 +1,7 @@
 /* eslint-env node */
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const Funnel = require('broccoli-funnel');
-const removeFile = require('broccoli-file-remover');
-const MergeTrees = require('broccoli-merge-trees');
+const mergeTrees = require('broccoli-merge-trees');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
@@ -59,7 +58,13 @@ module.exports = function(defaults) {
     getDestinationPath: () => '_headers'
   });
 
-  tree = removeFile(tree, { srcFile: 'netlify.headers' });
+  tree = new Funnel(tree, {
+    exclude: [
+      'netlify.headers',
+      'assets/vendor-*',
+      'assets/website-*.js'
+    ]
+  });
 
-  return new MergeTrees([tree, headersFile]);
+  return mergeTrees([tree, headersFile]);
 };
